bf94b50aa5796e1584cd1e3f3f902615b2c5a7c0
Laura <Laura@DESKTOP-BESSB6E>
2023-09-19 00:14:37 +0300

    Add project files.

---
+++ 2.3Uzduotis_Array/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace _2._3Uzduotis_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite sakini: ");
+            string sentence = Console.ReadLine();
+            Console.WriteLine(GetLastLetter(sentence));
+
+        }
+        public static char GetLastLetter(string sentence)
+        {
+            char[] chars = sentence.ToCharArray();
+            return chars[chars.Length - 1];
+
+        }
+    }
+}
